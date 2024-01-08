69796ce559fdee404086fe739539a6a61ce2170c
maxdyf <1048897180@qq.com>
2024-01-08 19:00:28 +0800

    1.重写了动画逻辑，将原来不稳定的setTimeout()方法改成基于Promise()函数的方式。 2.增加了速度调节的按钮，现在可以在界面上调节动画速度了。 3.优化代码逻辑。 	modified:   css/default.css 	modified:   index.html 	modified:   js/animate.js 	deleted:    js/jquery-3.5.0.min.js 	modified:   js/window-updater.js

--- js/animate.js
+++ js/animate.js
@@ -1,36 +1,58 @@
-//通过动画显示数字
-function showNumberWihthAnimation(i, j, randNumber) {
-    //console.log(i,j,randNumber);
-    let id = 'number-cell-' + i + '-' + j + '-1';
-    let cmd = '<div class="number-cell-' + randNumber + '" id="' + id + '" style="width:0px; height:0px; top:' + (getPosTop(i, j) + cellWidth / 2) + 'px; left:' + (getPosLeft(i, j) + cellWidth / 2) + 'px;"></div>';
-    console.log(cmd);
-    //console.log(cmd);
-    $("#grid-container").append(cmd);
-    let numberCell = $('#' + id);
-    numberCell.text(randNumber);
-    console.log(numberCell.css('top'));
-    console.log(numberCell.css('left'));
+var defaultAnimationTime = 100;
+var animationTime = defaultAnimationTime // 控制动画的时间
 
-    //console.log("Call Number Animation!\n");
-    //console.log(numberCell.html());
-    //console.log("(" + i + "," + j + "), value=" + randNumber);
-    numberCell.animate({
-        width: cellWidth,
-        height: cellWidth,
-        top: getPosTop(i, j),
-        left: getPosLeft(i, j)
-    }, 80);
+
+
+//通过动画显示数字
+function showNumberWithAnimation(i, j, randNumber) {
+    return new Promise((resolve) => {
+        let id = 'number-cell-' + i + '-' + j + '-1';
+        let cmd = `<div class="number-cell-${randNumber}" id="${id}" style="width: 0px; height: 0px; top: ${getPosTop(i, j) + cellWidth / 2}px; left: ${getPosLeft(i, j) + cellWidth / 2}px;"></div>`;
+        
+        //console.log(cmd);
+
+        $("#grid-container").append(cmd);
+        let numberCell = $('#' + id);
+        numberCell.text(randNumber);
+
+        //console.log(numberCell.css('top'));
+        //console.log(numberCell.css('left'));
+
+        animateCounter++;
+        $(numberCell).animate({
+            width: cellWidth,
+            height: cellWidth,
+            top: getPosTop(i, j),
+            left: getPosLeft(i, j)
+        }, {
+            duration: animationTime,
+            complete: () => {
+                animateCounter--;
+                console.log(animationTime);
+                resolve();
+            } // 当动画完成时解决 Promise
+        });
+    });
 }
+
+
 
 //通过动画显示移动的效果
 function showMoveAnimation(fromx, fromy, tox, toy) {
-    let numberCell = $('#number-cell-' + fromx + '-' + fromy);
-    //console.log(numberCell.attr('class'));
-    //console.log("Call Animation!\n");
-    //console.log(numberCell.html());
-    //console.log("from (" + fromx + ',' + fromy + "), to (" + tox + "," + toy + ").")
-    numberCell.animate({
-        top: getPosTop(tox, toy),
-        left: getPosLeft(tox, toy)
-    }, 80);
+    return new Promise((resolve) => {
+        
+        let numberCell = $('#number-cell-' + fromx + '-' + fromy);
+        animateCounter++;
+        $(numberCell).animate({
+            top: getPosTop(tox, toy),
+            left: getPosLeft(tox, toy)
+        }, {
+            duration: animationTime,
+            complete: () => {
+                console.log(animationTime);
+                animateCounter--;
+                resolve();
+            } // 当动画完成时解决 Promise
+        });
+    });
 }
--- js/window-updater.js
+++ js/window-updater.js
@@ -1,9 +1,8 @@
-//移动端尺寸
 var documentWidth=document.documentElement.clientWidth;//页面DOM的宽度
 var containerWidth=documentWidth*0.92;//容器的宽度
 var cellWidth=documentWidth*0.18;
 var cellSpace=documentWidth*0.04;
-
+var animateCounter = 0; // 定义动画计数器，用于统计正在进行的动画
 const N = 4, M = 4;
 var board = new Board(N, M);
 console.log(documentWidth,containerWidth,cellWidth,cellSpace);
@@ -27,60 +26,83 @@ function newgame(){
             $(gridCell).css('left', getPosLeft(i, j));
         }
     board.reset();
-    updateView(board, []);
-
-	//在随机的两个单元格生成数字
-    setTimeout(function(){
+    updateView(board, [])
+    .then(() => {
+        //在随机的两个单元格生成数字
         let vec1 = board.randomSummon();
         let vec2 = board.randomSummon();
         updateView(board, [vec1, vec2]);
-    }, 200);
-
+    });
 }
-
-function updateView(board, new_summon)
-{
-    let vec = board.moveTrack;
-    for (let i = 0; i < board.moveTrack.length; i++)
-        showMoveAnimation(vec[i][0], vec[i][1], vec[i][2], vec[i][3]);
-    setTimeout(function()
-    {
-        for (let i = 0; i < new_summon.length; i++)
-            showNumberWihthAnimation(new_summon[i][0], new_summon[i][1], new_summon[i][2]);
-    }, 60);
-    setTimeout(function(){
-        for (let i = 0; i < N; i++)
-        {
-            for (let j = 0; j < M; j++)
-            {
-                $("#number-cell-" + i + '-' + j).remove();
-                $("#number-cell-" + i + '-' + j + '-1').remove();
-                let cmd = '<div class="number-cell-' + board.board[i][j] + '" id="number-cell-' + i + '-' + j + '"></div>';
-                //console.log(cmd);
-                $("#grid-container").append(cmd);
-                let numberCell = $("#number-cell-" + i + '-' + j);
-                
-                let x = board.board[i][j];
-                if (x > 0)
-                {
-                    numberCell.css('top', getPosTop(i, j));
-                    numberCell.css('left', getPosLeft(i, j));
-                    numberCell.css('width', cellWidth);
-                    numberCell.css('height', cellWidth);
-                    numberCell.text(board.board[i][j]);
-                }
-                else
-                {
-                    numberCell.css('top', getPosTop(i, j) + cellWidth / 2);
-                    numberCell.css('left', getPosLeft(i, j) + cellWidth / 2);
-                    numberCell.css('width', 0);
-                    numberCell.css('height', 0);  
-                }
-            }
+function updateView(board, new_summon = [], merged = []) {
+    return new Promise((resolve) => {
+        let promiseMoveAnimation = [];
+        // 开始更新有位移的动画
+        for (let i = 0; i < board.moveTrack.length; i++) {
+            promiseMoveAnimation.push(showMoveAnimation(board.moveTrack[i][0], board.moveTrack[i][1], board.moveTrack[i][2], board.moveTrack[i][3]));
         }
-        $("#score").text(board.score);
-    }, 160);
+
+        Promise.all(promiseMoveAnimation)
+            .then(() => {
+                // 开始更新合并后数字的动画
+                for (let i = 0; i < merged.length; i++)
+                {
+                    let x = merged[i][0], y = merged[i][1], val = merged[i][2];
+                    $("#number-cell-" + x + '-' + y).remove();
+                    let cmd = '<div class="number-cell-' + val + '" id="number-cell-' + x + '-' + y + '"></div>';
+                    console.log(cmd);
+                    $("#grid-container").append(cmd);
+                    let numberCell = $("#number-cell-" + x + '-' + y);
+                    numberCell.css('top', getPosTop(x, y));
+                    numberCell.css('left', getPosLeft(x, y));
+                    numberCell.css('width', 0);
+                    numberCell.css('height', 0);
+                    numberCell.text(val);
+                }
+                // 开始更新新出现的数字动画
+                let promiseNumAnimation = [];
+
+                for (let i = 0; i < new_summon.length; i++) {
+                    promiseNumAnimation.push(showNumberWithAnimation(new_summon[i][0], new_summon[i][1], new_summon[i][2]));
+                }
+
+                return Promise.all(promiseNumAnimation);
+            })
+            .then(() => {
+                for (let i = 0; i < N; i++) {
+                    for (let j = 0; j < M; j++) {
+                        $("#number-cell-" + i + '-' + j).remove();
+                        $("#number-cell-" + i + '-' + j + '-1').remove();
+                        let cmd = '<div class="number-cell-' + board.board[i][j] + '" id="number-cell-' + i + '-' + j + '"></div>';
+                        $("#grid-container").append(cmd);
+                        let numberCell = $("#number-cell-" + i + '-' + j);
+
+                        let x = board.board[i][j];
+
+                        if (x > 0) {
+                            numberCell.css('top', getPosTop(i, j));
+                            numberCell.css('left', getPosLeft(i, j));
+                            numberCell.css('width', cellWidth);
+                            numberCell.css('height', cellWidth);
+                            numberCell.text(board.board[i][j]);
+                        } else {
+                            numberCell.css('top', getPosTop(i, j) + cellWidth / 2);
+                            numberCell.css('left', getPosLeft(i, j) + cellWidth / 2);
+                            numberCell.css('width', 0);
+                            numberCell.css('height', 0);
+                        }
+                    }
+                }
+                $("#score").text(board.score);
+                resolve();
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                resolve(); // Ensure the Promise is resolved even in case of an error
+            });
+    });
 }
+
 function isGameOver()
 {
     if (board.getStatus())
@@ -94,18 +116,42 @@ function isGameOver()
 
 
 
-// 引入节流函数，限制键盘输入频率，以达到控制动画不出错的效果。
-const handleKeyboardEventThrottled = $.throttle(200, function(event)
+const startKeyboardExecution = (function(event)
 {
-    event.preventDefault();
+    //console.log(animateCounter);
+    if (animateCounter > 0)
+        return;
     if (board.getStatus())
         return;
     //console.table(board.board);
+    let inputSpeed = $("#speed");
+    if (inputSpeed.val() == '')
+        return;
+    let val = parseFloat(inputSpeed.val());
+    if($.isNumeric(val) == false)
+    {
+        alert("Speed must be an NUMBER!");
+        animationTime = defaultAnimationTime;
+        inputSpeed.val(defaultAnimationTime);
+        return;
+    }
+    else if (val > 1000 || val < 0)
+    {
+        alert("Too Big or too small!");
+        animateTime = defaultAnimationTime;
+        inputSpeed.val(defaultAnimationTime);
+        return;
+    }
+    else
+        animationTime = val;
+    let result = [], merged = [];
     switch(event.key)
     {
         case 'ArrowUp':
+            event.preventDefault();
             console.log("ArrowUp!\n");
-            if (board.moveVertical(-1) == true)
+            result = board.moveVertical(-1);
+            if (result[0] == true)
             {
                 console.log("Can't Move Up!\n");
                 console.table(board.board);
@@ -114,8 +160,10 @@ const handleKeyboardEventThrottled = $.throttle(200, function(event)
             break;
 
         case 'ArrowDown':
+            event.preventDefault();
             console.log("ArrowDown!\n");
-            if (board.moveVertical(1) == true)
+            result = board.moveVertical(1);
+            if (result[0] == true)
             {
                 console.log("Can't Move Down!\n");
                 console.table(board.board);
@@ -124,8 +172,10 @@ const handleKeyboardEventThrottled = $.throttle(200, function(event)
             break;
 
         case 'ArrowLeft':
+            event.preventDefault();
             console.log("ArrowLeft!\n");
-            if (board.moveHorizontal(-1) == true)
+            result = board.moveHorizontal(-1);
+            if (result[0] == true)
             {
                 console.log("Can't Move Left!\n");
                 console.table(board.board);
@@ -134,8 +184,10 @@ const handleKeyboardEventThrottled = $.throttle(200, function(event)
             break;
 
         case 'ArrowRight':
+            event.preventDefault();
             console.log("ArrowRight!\n");
-            if (board.moveHorizontal(1) == true)
+            result = board.moveHorizontal(1);
+            if (result[0] == true)
             {
                 console.log("Can't Move Right!\n");
                 console.table(board.board);
@@ -145,12 +197,15 @@ const handleKeyboardEventThrottled = $.throttle(200, function(event)
         default:
             return;
     }
+    merged = result[1];
     let vec = board.randomSummon();
-    updateView(board, [vec]);
-    isGameOver();
+    updateView(board, [vec], merged)
+    .then(isGameOver)
 });
-document.addEventListener('keydown', handleKeyboardEventThrottled);
+document.addEventListener('keydown', startKeyboardExecution);
 document.addEventListener('DOMContentLoaded',function()
 {
+    let inputSpeed = $("#speed");
+    inputSpeed.val(defaultAnimationTime);
     newgame();
 });
